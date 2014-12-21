20316bc303ae3f403203076350a8060bc5743835
Johnny Parizek <JohnnyParizek@Pentwater-iMac-2012.local>
2014-12-20 19:08:41 -0500

    Initial Commit

---
+++ dayOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dayOne
+//
+//  Created by Johnny Parizek on 12/20/14.
+//  Copyright (c) 2014 Johnny Parizek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
